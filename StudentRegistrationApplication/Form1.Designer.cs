b4454f5692a80a25e99c36836a7494a638efe678
Wormhole16 <judelfederigan1@gmail.com>
2022-02-10 15:54:20 +0800

    Student Registration Application

---
+++ StudentRegistrationApplication/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace StudentRegistrationApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LastName = new System.Windows.Forms.TextBox();
+            this.FirstName = new System.Windows.Forms.TextBox();
+            this.MiddleName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Male = new System.Windows.Forms.RadioButton();
+            this.Female = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Day = new System.Windows.Forms.ComboBox();
+            this.Month = new System.Windows.Forms.ComboBox();
+            this.Year = new System.Windows.Forms.ComboBox();
+            this.RegisterStudent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Registration Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last name *";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First name *";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Middle name *";
+            // 
+            // LastName
+            // 
+            this.LastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastName.Location = new System.Drawing.Point(16, 84);
+            this.LastName.Name = "LastName";
+            this.LastName.Size = new System.Drawing.Size(274, 26);
+            this.LastName.TabIndex = 4;
+            // 
+            // FirstName
+            // 
+            this.FirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstName.Location = new System.Drawing.Point(16, 141);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(274, 26);
+            this.FirstName.TabIndex = 5;
+            // 
+            // MiddleName
+            // 
+            this.MiddleName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MiddleName.Location = new System.Drawing.Point(16, 205);
+            this.MiddleName.Name = "MiddleName";
+            this.MiddleName.Size = new System.Drawing.Size(274, 26);
+            this.MiddleName.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Gender *";
+            // 
+            // Male
+            // 
+            this.Male.AutoSize = true;
+            this.Male.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Male.Location = new System.Drawing.Point(103, 239);
+            this.Male.Name = "Male";
+            this.Male.Size = new System.Drawing.Size(61, 24);
+            this.Male.TabIndex = 8;
+            this.Male.TabStop = true;
+            this.Male.Text = "Male";
+            this.Male.UseVisualStyleBackColor = true;
+            // 
+            // Female
+            // 
+            this.Female.AutoSize = true;
+            this.Female.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Female.Location = new System.Drawing.Point(170, 239);
+            this.Female.Name = "Female";
+            this.Female.Size = new System.Drawing.Size(80, 24);
+            this.Female.TabIndex = 9;
+            this.Female.TabStop = true;
+            this.Female.Text = "Female";
+            this.Female.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 284);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Date of birth *";
+            // 
+            // Day
+            // 
+            this.Day.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Day.FormattingEnabled = true;
+            this.Day.Location = new System.Drawing.Point(17, 307);
+            this.Day.Name = "Day";
+            this.Day.Size = new System.Drawing.Size(80, 28);
+            this.Day.TabIndex = 11;
+            this.Day.Text = "-Day-";
+            // 
+            // Month
+            // 
+            this.Month.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Month.FormattingEnabled = true;
+            this.Month.Location = new System.Drawing.Point(103, 307);
+            this.Month.Name = "Month";
+            this.Month.Size = new System.Drawing.Size(81, 28);
+            this.Month.TabIndex = 12;
+            this.Month.Text = "-Month-";
+            // 
+            // Year
+            // 
+            this.Year.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Year.FormattingEnabled = true;
+            this.Year.Location = new System.Drawing.Point(190, 307);
+            this.Year.Name = "Year";
+            this.Year.Size = new System.Drawing.Size(100, 28);
+            this.Year.TabIndex = 13;
+            this.Year.Text = "-Year-";
+            // 
+            // RegisterStudent
+            // 
+            this.RegisterStudent.BackColor = System.Drawing.Color.Crimson;
+            this.RegisterStudent.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RegisterStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterStudent.ForeColor = System.Drawing.Color.White;
+            this.RegisterStudent.Location = new System.Drawing.Point(17, 341);
+            this.RegisterStudent.Name = "RegisterStudent";
+            this.RegisterStudent.Size = new System.Drawing.Size(176, 37);
+            this.RegisterStudent.TabIndex = 14;
+            this.RegisterStudent.Text = "Register student";
+            this.RegisterStudent.UseVisualStyleBackColor = false;
+            this.RegisterStudent.Click += new System.EventHandler(this.RegisterStudent_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 386);
+            this.Controls.Add(this.RegisterStudent);
+            this.Controls.Add(this.Year);
+            this.Controls.Add(this.Month);
+            this.Controls.Add(this.Day);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Female);
+            this.Controls.Add(this.Male);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.MiddleName);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.LastName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Student Registration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox LastName;
+        private System.Windows.Forms.TextBox FirstName;
+        private System.Windows.Forms.TextBox MiddleName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton Male;
+        private System.Windows.Forms.RadioButton Female;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox Day;
+        private System.Windows.Forms.ComboBox Month;
+        private System.Windows.Forms.ComboBox Year;
+        private System.Windows.Forms.Button RegisterStudent;
+    }
+}
+
---
+++ StudentRegistrationApplication/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StudentRegistrationApplication
+{   
+    public partial class Form1 : Form
+    {
+        string Gender = "";   
+        int BirthDay, BirthMonth, BirthYear;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            for (int i=1;i<=31;i++)
+            {
+                Day.Items.Add(i);
+            }
+            for (int i = 1; i <= 12; i++)
+            {
+                Month.Items.Add(i);
+            }
+            for (int i=1900;i<=2021;i++)
+            {
+                Year.Items.Add(i);
+            }
+           
+        }
+
+        private void RegisterStudent_Click(object sender, EventArgs e)
+        {
+            if (Male.Checked) Gender = Male.Text;
+            else Gender = Female.Text;
+
+            if (Day.Text != "-Day-" && Month.Text != "-Month-" && Year.Text != "-Year-")
+            {
+                BirthDay = (int)Day.SelectedItem;
+                BirthMonth = (int)Month.SelectedItem;
+                BirthYear = (int)Year.SelectedItem;
+            }
+
+            MessageBox.Show("Student name: " + FirstName.Text + " " + LastName.Text + " " +
+                                               MiddleName.Text + "\nGender: " + 
+                                               Gender + "\nDate of birth: " + 
+                                               BirthDay + "/" + BirthMonth + "/" + BirthYear);
+        }
+    }
+}
